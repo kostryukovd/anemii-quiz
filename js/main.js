043d8b5577e8ff81f23ba6dca1b7c8eb8a0edff1
kostryukovd <kostryukovd@gmail.com>
2018-11-15 12:34:22 +0300

    переделан тест на радиокнопки

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 
 
 
+
+
 function expand_menu() {
 
 console.log("test");
@@ -9,6 +11,44 @@ $('.header-menu__lines').toggleClass("header-menu__lines-rotate");
 
 $('.hidden-menu').toggleClass("hidden-menu__show");
 
+}
+
+function answerClicked(n) {
+
+    if($('#input'+n).prop('checked') == false) {
+        $('#input'+n).prop('checked',true)
+    }
+
+    for(i=1;i<=5;i++) {
+        if (i!=n) {
+            $('#input'+i).prop('checked',false);
+        }
+    }
+}
+
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function resetQuiz() {
+	for(i=1;i<=6;i++) {
+		for(j=1;j<=5;j++) {
+			document.cookie = i + '_' + j + '=0';
+		}
+	}
 }
 
 
